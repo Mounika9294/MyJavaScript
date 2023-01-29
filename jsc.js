119f6ba77836e7890d40d60313ff41c906e3dd24
Mounika9294 <123893840+Mounika9294@users.noreply.github.com>
2023-01-29 23:06:13 +0530

    Create jsc.js

---
+++ jsc.js
@@ -0,0 +1,6 @@
+function changeStyle() {
+    var clr = document.getElementById("para1").style.color = "white";
+    var bgclr = document.getElementById("para1").style.backgroundColor = "blue";
+    var fnt = document.getElementById("para1").style.fontStyle = "Italic";
+
+}
